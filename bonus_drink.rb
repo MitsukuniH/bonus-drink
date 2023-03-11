975025fd740d1089b41905b7e5727ec4156d7f00
MitsukuniH <hmitsuku3292@gmail.com>
2023-03-11 19:57:15 +0900

    solved question

--- bonus_drink.rb
+++ bonus_drink.rb
@@ -1,5 +1,16 @@
 class BonusDrink
   def self.total_count_for(amount)
-    0
+    sum = amount
+    bonus = amount / 3
+    remain = amount % 3
+    while bonus != 0 do
+      sum += bonus
+      temp = bonus
+      bonus = (temp+remain) / 3
+      remain = (temp+remain) % 3
+    end
+    return sum
   end
 end
+
+puts BonusDrink.total_count_for(100)
